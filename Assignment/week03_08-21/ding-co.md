23f8cda2ea7568df481e06c6e14f25f59e0cc5d1
ding-co <pangoons@naver.com>
2021-08-23 18:44:02 +0900

    Add Assignment week03

---
+++ Assignment/week03_08-21/ding-co.md
@@ -0,0 +1,42 @@
+## 싹 풀스택 8월 3주차 과제 제출 (김민수)
+
+### 제출기한: 21.08.28 토
+
+#
+
+### _과제 - 숫자 야구 게임_
+
+- 컴퓨터가 숫자 3개 먼저 정함 (난수 3개, random 함수 이용)
+  이후 사용자가 숫자 3개 입력
+
+- 3자리 수로 만들기 (숫자 중복 불가, 각 숫자는 한번씩 사용 가능, 0은 빼도 되고 넣어도 됨)
+- 라운드는 9라운드까지
+- 숫자와 자리의 위치가 맞으면 Strike <br/>
+  숫자만 맞으면 Ball<br/>
+  숫자가 하나도 맞지 않을 경우 Out
+
+- 예시) <br/>
+  컴퓨터: 2 3 5 <br/>
+  사용자: 1 2 3 ⇒ 2B <br/>
+  사용자: 2 1 4 ⇒ 1S <br/>
+  3 strike 나오면 사용자 승리 → 이후 종료 or 게임 재시작
+
+- 제출 방법 <br/>
+
+  - 내가 만든 홈페이지에 index.html를 올려놓거나 <br/> baseball.js 로 짜서 node로 실행
+
+### _시연_
+
+- https://ding-co.topician.com/
+
+### _src_
+
+- [index.html](https://github.com/ding-co/ssac-web-fullstack/blob/main/Assignment/week03_08-21/src/index.html)
+
+- [css/styles.css](https://github.com/ding-co/ssac-web-fullstack/blob/main/Assignment/week03_08-21/src/css/styles.css)
+
+- [css/reset.css](https://github.com/ding-co/ssac-web-fullstack/blob/main/Assignment/week03_08-21/src/reset.css)
+
+- [css/screens/baseball.css](https://github.com/ding-co/ssac-web-fullstack/blob/main/Assignment/week03_08-21/src/css/screens/baseball.css)
+
+- [js/baseball.js](https://github.com/ding-co/ssac-web-fullstack/blob/main/Assignment/week03_08-21/src/js/baseball.js)
---
+++ Assignment/week03_08-21/src/js/baseball.js
@@ -0,0 +1,119 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const startBtn = document.querySelector('.baseball-game__start');
+  const resetBtn = document.querySelector('.baseball-game__reset');
+  const inputForm = document.querySelector('#baseball-form');
+  const inputGuess = document.querySelector('.baseball-form__text');
+  const resultGuess = document.querySelector('.baseball-form__guess');
+  const resultRound = document.querySelector('.baseball-form__result');
+
+  const HIDDEN_CLASS = 'hidden';
+
+  let answer = '';
+  let guessNumber = '';
+
+  let round = 0;
+  let strike = 0;
+  let ball = 0;
+  let out = 0;
+
+  const createAnswer = function () {
+    const numberCandidate = new Set();
+    while (answer.length !== 3) {
+      const element = Math.floor(Math.random() * 10);
+      if (numberCandidate.has(element)) continue;
+      numberCandidate.add(element);
+      answer += element.toString();
+    }
+    return answer;
+  };
+
+  const handleStartBtn = function () {
+    inputForm.classList.remove(HIDDEN_CLASS);
+    if (answer === '') {
+      answer = createAnswer();
+      alert('게임을 시작하겠습니다.');
+      console.log(answer);
+    } else if (answer === guessNumber || round > 9) {
+      alert('이미 게임이 끝났습니다.');
+      console.log(answer);
+    } else {
+      alert('이미 게임이 시작되었습니다.');
+      console.log(answer);
+    }
+  };
+
+  const handleResetBtn = function () {
+    // To Do: 새로고침 안하고 form만 초기화 하면서 hidden add하기
+    window.location.reload();
+  };
+
+  const checkNumber = function (number = '') {
+    const guessSet = new Set(number);
+    if (guessSet.size === 3) {
+      return true;
+    }
+    alert('중복된 숫자가 있습니다!');
+    return false;
+  };
+
+  const checkStrike = function (number = '') {
+    answerArray = answer.split('');
+    guessArray = number.split('');
+    strike = 0;
+    for (let i = 0; i < number.length; i++) {
+      if (answerArray[i] === guessArray[i]) {
+        strike += 1;
+      }
+    }
+    return strike;
+  };
+
+  const checkBall = function (number = '') {
+    answerArray = answer.split('');
+    guessArray = number.split('');
+    ball = 0;
+    for (let i = 0; i < number.length; i++) {
+      if (
+        answerArray[i] !== guessArray[i] &&
+        guessArray.includes(answerArray[i])
+      ) {
+        ball += 1;
+      }
+    }
+    return ball;
+  };
+
+  const checkOut = function (number = '') {
+    if (checkStrike(number) === 0 && checkBall(number) === 0) {
+      return out + 1;
+    }
+    return out;
+  };
+
+  const handleInputForm = function (event) {
+    event.preventDefault();
+    guessNumber = inputGuess.value;
+
+    if (checkNumber(guessNumber)) {
+      round += 1;
+      strike = checkStrike(guessNumber);
+      ball = checkBall(guessNumber);
+      out = checkOut(guessNumber);
+
+      if (guessNumber === answer && strike === 3) {
+        resultGuess.textContent = '';
+        resultRound.textContent = 'You win!';
+      } else if (round <= 9 && out < 3) {
+        resultGuess.textContent = `Your Guess Number is ${guessNumber}`;
+        resultRound.textContent = `Round ${round}: ${strike} Strike | ${ball} Ball | ${out} Out`;
+      } else {
+        resultGuess.textContent = '';
+        resultRound.textContent = 'You lose!';
+      }
+    }
+  };
+
+  startBtn.addEventListener('click', handleStartBtn);
+  resetBtn.addEventListener('click', handleResetBtn);
+  inputForm.addEventListener('submit', handleInputForm);
+});
