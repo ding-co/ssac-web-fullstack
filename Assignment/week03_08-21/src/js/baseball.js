008278d40401c3fe41427e5347679f10bd9d9ff7
ding-co <pangoons@naver.com>
2021-09-07 09:01:26 +0900

    Update Assignment week03 src/js

--- Assignment/week03_08-21/src/js/baseball.js
+++ Assignment/week03_08-21/src/js/baseball.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // Module Pattern
   const Dom = (function () {
     const startBtn = document.querySelector('.baseball-game__start');
     const resetBtn = document.querySelector('.baseball-game__reset');
@@ -29,58 +28,94 @@ document.addEventListener('DOMContentLoaded', () => {
   const HIDDEN_CLASS = 'hidden';
   const ANSWER_SIZE = 3;
 
-  let answerNumber = '';
-  let guessNumber = '';
+  let gAnswerNumber;
+  let gGuessNumber;
 
-  let roundCnt = 0;
-  let strikeCnt = 0;
-  let ballCnt = 0;
-  let outCnt = 0;
+  let gRoundCnt = 0;
+  let gStrikeCnt = 0;
+  let gBallCnt = 0;
+  let gOutCnt = 0;
 
-  const createAnswer = function () {
-    // Array
-    const numberCandidates = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    let numberCandidatesLength = 10;
-    for (let i = 0; i < ANSWER_SIZE; i++) {
-      const randomNumberIndex = Math.floor(
-        Math.random() * numberCandidatesLength
-      );
-      const randomNumber = numberCandidates[randomNumberIndex];
-      answerNumber += randomNumber.toString();
-      numberCandidates.splice(randomNumberIndex, 1);
-      numberCandidatesLength -= 1;
-    }
-    return answerNumber;
-  };
-
-  const handleStartBtn = function () {
+  const handleStartBtn = () => {
     Dom.getInputForm().classList.remove(HIDDEN_CLASS);
-    // answerNumber === ''
-    if (!answerNumber) {
-      answerNumber = createAnswer();
+    if (!gAnswerNumber) {
+      gAnswerNumber = createAnswer();
+      console.log(gAnswerNumber);
       alert('게임을 시작하겠습니다.');
-      console.log(answerNumber);
-    } else if (answerNumber === guessNumber || roundCnt > 9 || outCnt === 3) {
+    } else if (
+      gAnswerNumber === gGuessNumber ||
+      gRoundCnt > 9 ||
+      gOutCnt === 3
+    ) {
       alert('이미 게임이 끝났습니다.');
     } else {
       alert('이미 게임이 시작되었습니다.');
     }
   };
 
-  const handleResetBtn = function () {
+  const createAnswer = () => {
+    gAnswerNumber = '';
+    const numberCandidates = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    for (let i = 0; i < ANSWER_SIZE; i++) {
+      const randomNumberIndex = Math.floor(
+        Math.random() * numberCandidates.length
+      );
+      const randomNumber = numberCandidates[randomNumberIndex];
+      gAnswerNumber += randomNumber.toString();
+      deleteRandomNumber(numberCandidates, randomNumberIndex);
+    }
+    return gAnswerNumber;
+  };
+
+  const deleteRandomNumber = (array, index) => {
+    for (let i = index; i < array.length - 1; i++) {
+      array[i] = array[i + 1];
+    }
+    array.length -= 1;
+  };
+
+  const handleResetBtn = () => {
+    resetDom();
+    resetGlobalVars();
+  };
+
+  const resetDom = () => {
     Dom.getInputForm().classList.add(HIDDEN_CLASS);
     Dom.getInputGuess().value = '';
     Dom.getResultGame().innerHTML = '';
-    answerNumber = '';
-    guessNumber = '';
-    roundCnt = 0;
-    strikeCnt = 0;
-    ballCnt = 0;
-    outCnt = 0;
   };
 
-  const checkNumber = function (userGuessNumber = '') {
-    // Set
+  const resetGlobalVars = () => {
+    gAnswerNumber = '';
+    gGuessNumber = '';
+    gRoundCnt = 0;
+    gStrikeCnt = 0;
+    gBallCnt = 0;
+    gOutCnt = 0;
+  };
+
+  const handleInputForm = (event) => {
+    event.preventDefault();
+    gGuessNumber = Dom.getInputGuess().value;
+
+    if (checkNumber(gGuessNumber)) {
+      gRoundCnt += 1;
+      gStrikeCnt = checkStrike(gGuessNumber);
+      gBallCnt = checkBall(gGuessNumber);
+      gOutCnt = checkOut();
+
+      if (gGuessNumber === gAnswerNumber || gStrikeCnt === 3) {
+        Dom.getResultGame().innerHTML = `Yeah, the answer was ${gAnswerNumber}.<br/>You win!`;
+      } else if (gRoundCnt <= 9 && gOutCnt < 3) {
+        Dom.getResultGame().innerHTML = `Your Guess Number is ${gGuessNumber}.<br/>
+          Round ${gRoundCnt}: ${gStrikeCnt} Strike | ${gBallCnt} Ball | ${gOutCnt} Out`;
+      } else {
+        Dom.getResultGame().innerHTML = `Sorry, the answer was ${gAnswerNumber}.<br/>You lose!`;
+      }
+    }
+  };
+
+  const checkNumber = (userGuessNumber) => {
     const guessSet = new Set(userGuessNumber);
     if (isNaN(Number(userGuessNumber))) {
       alert('숫자만 입력해주세요!');
@@ -92,57 +127,34 @@ document.addEventListener('DOMContentLoaded', () => {
     return true;
   };
 
-  const checkStrike = function (userGuessNumber = '') {
-    strikeCnt = 0;
+  const checkStrike = (userGuessNumber) => {
+    gStrikeCnt = 0;
     for (let i = 0; i < ANSWER_SIZE; i++) {
-      if (answerNumber.charAt(i) === userGuessNumber.charAt(i)) {
-        strikeCnt += 1;
+      if (gAnswerNumber[i] === userGuessNumber[i]) {
+        gStrikeCnt += 1;
       }
     }
-    return strikeCnt;
+    return gStrikeCnt;
   };
 
-  const checkBall = function (userGuessNumber = '') {
-    const answerArray = answerNumber.split('');
-    const guessArray = userGuessNumber.split('');
-    ballCnt = 0;
+  const checkBall = (userGuessNumber) => {
+    gBallCnt = 0;
     for (let i = 0; i < ANSWER_SIZE; i++) {
       if (
-        answerArray[i] !== guessArray[i] &&
-        answerArray.includes(guessArray[i])
+        gAnswerNumber[i] === userGuessNumber[(i + 1) % 3] ||
+        gAnswerNumber[i] === userGuessNumber[(i + 2) % 3]
       ) {
-        ballCnt += 1;
+        gBallCnt += 1;
       }
     }
-    return ballCnt;
+    return gBallCnt;
   };
 
-  const checkOut = function () {
-    if (strikeCnt === 0 && ballCnt === 0) {
-      return outCnt + 1;
+  const checkOut = () => {
+    if (gStrikeCnt === 0 && gBallCnt === 0) {
+      return gOutCnt + 1;
     }
-    return outCnt;
-  };
-
-  const handleInputForm = function (event) {
-    event.preventDefault();
-    guessNumber = Dom.getInputGuess().value;
-
-    if (checkNumber(guessNumber)) {
-      roundCnt += 1;
-      strikeCnt = checkStrike(guessNumber);
-      ballCnt = checkBall(guessNumber);
-      outCnt = checkOut();
-
-      if (guessNumber === answerNumber || strikeCnt === 3) {
-        Dom.getResultGame().innerHTML = `Yeah, the answer was ${answerNumber}.<br/>You win!`;
-      } else if (roundCnt <= 9 && outCnt < 3) {
-        Dom.getResultGame().innerHTML = `Your Guess Number is ${guessNumber}.<br/>
-          Round ${roundCnt}: ${strikeCnt} Strike | ${ballCnt} Ball | ${outCnt} Out`;
-      } else {
-        Dom.getResultGame().innerHTML = `Sorry, the answer was ${answerNumber}.<br/>You lose!`;
-      }
-    }
+    return gOutCnt;
   };
 
   Dom.getStartBtn().addEventListener('click', handleStartBtn);
